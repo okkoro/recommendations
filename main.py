b14743396f8b799dfc4f57bc973ccf5c6cb8ee53
Clément Cadieux <cadieuxclement@gmail.com>
2023-01-10 11:34:30 -0500

    Slight modifications

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ movies_ref = db.collection(u"movies")
 
 # For now, I know id 100 exists so I'll just get that
 
+
 @api.route("/api/recommendation")
 def getMovie():
     movie = movies_ref.document("100").get()
-    print(movie.to_dict())
     return jsonify(movie.to_dict()), 200
